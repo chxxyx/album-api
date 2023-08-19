e5bebdf628c777105ada152da4663c3d289b4a34
chxxyx <codud0826@naver.com>
2023-08-19 23:53:05 +0900

    feat: main 루트 routes 설정

---
+++ routes/user.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import controller from '../controller/UserController';
+const router = express.Router();
+
+router.get('/', controller.main); //
+
+export default router;
